334c17cbffb9330b419a3a53aa2ca07f7c821126
mig1098 <mig1098@hotmail.com>
2016-04-13 09:31:37 -0500

    update

---
+++ Library/Api.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PHP client for connecting to the Bigcommerce V2 REST Bigcommerce.
+ *
+ * Only use this file if you don't want autoloading or package management.
+ * The new api no need set CIPHER
+ */
+require_once BASE_APPPATH. 'Library/Bigcommerce/Connection.php';
+require_once BASE_APPPATH. 'Library/Bigcommerce/Error.php';
+//require_once BASE_APPPATH. 'Library/Bigcommerce/ClientError.php';
+require_once BASE_APPPATH. 'Library/Bigcommerce/ServerError.php';
+require_once BASE_APPPATH. 'Library/Bigcommerce/NetworkError.php';
