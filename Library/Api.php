2edeff31c148919d32003472664ec745801e878e
miglio esaud <mig1098@hotmail.com>
2016-04-22 09:35:33 -0500

    Update Api.php

--- Library/Api.php
+++ Library/Api.php
@@ -6,6 +6,7 @@
  * Only use this file if you don't want autoloading or package management.
  * The new api no need set CIPHER
  */
+define('STDIN' , fopen("php://stdin","r"));
 require_once  'Library/Bigcommerce/Connection.php';
 require_once  'Library/Bigcommerce/Error.php';
 require_once  'Library/Bigcommerce/ClientError.php';
