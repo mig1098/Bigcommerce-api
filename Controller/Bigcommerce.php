4e35653cbef58b28feacbf953d36001d9c45c89e
miglio esaud <mig1098@hotmail.com>
2016-04-22 09:07:33 -0500

    Update Bigcommerce.php

--- Controller/Bigcommerce.php
+++ Controller/Bigcommerce.php
@@ -5,15 +5,26 @@ class Bigcommerce {
     private $bigcommerce;
     
     public function __construct(){
+    	//private api
         $this->bigcommerce = new bigcomModel(array(
             'type'     => 'private',
             'username' => 'your-user',
             'api_path' => 'https://your-store.mybigcommerce.com/api/v2/',
 	    'api_token'=>'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
         ));
+        /*
+        //public api
+        $this->bigcommerce = new bigcomModel(array(
+            'type'=>'public',
+            'context'=>'stores/your-store-context',
+            'access_token'=>'your-access-token'
+        ));
+        */
     }
     
-    public function execute(){
-    	print_r($this->bigcommerce->getStoreInformation());
+    public function execute($callback){
+        if(is_callable($callback)){
+            $callback($this);
+        }
     }
 }
