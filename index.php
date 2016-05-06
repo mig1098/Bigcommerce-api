41a02b836588b9a22facb58f9a1b9b9990a4eedc
miglio esaud <mig1098@hotmail.com>
2016-05-06 12:00:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 include './Library/Api.php';
 include './Model/Bigcommerce.php';
 include './Controller/Bigcommerce.php';
-/**
-begin api
-**/
+
+//begin api
 $bigcommerce = new \Controller\Bigcommerce();
 $bigcommerce->execute(function(\Controller\Bigcommerce $controller){
     print_r($controller->bigcommerce->getStoreInformation());
